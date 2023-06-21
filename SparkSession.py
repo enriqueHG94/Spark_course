f4cf24de4fdd1c59d4a502375ca549baf151a233
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-06-21 16:53:28 +0200

    I have created the SparkSession and the SparkContext

---
+++ SparkSession.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+        .master("local[*]")\
+        .appName('PySpark_training')\
+        .getOrCreate()
+
+sc = spark.sparkContext
